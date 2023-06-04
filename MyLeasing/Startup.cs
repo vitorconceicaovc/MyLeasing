6bc99d88ddb33096f93ffe720632c1d3de4134c4
Vítor Conceição <vitoralmeidavc@outlook.pt>
2023-06-04 23:18:29 +0100

    CRUD Owners

--- MyLeasing/Startup.cs
+++ MyLeasing/Startup.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MyLeasing.Web.Data;
 
 namespace MyLeasing
 {
@@ -23,7 +25,13 @@ namespace MyLeasing
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
+
+			services.AddDbContext<DataContext>(cfg =>
+			{
+				cfg.UseSqlServer(this.Configuration.GetConnectionString("DefaultConnection"));
+			});	
 			services.AddControllersWithViews();
+
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
