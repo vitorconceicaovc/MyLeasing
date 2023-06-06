48c010b50172a5ff67c01bc0b757d7b379f35aee
Vítor Conceição <vitoralmeidavc@outlook.pt>
2023-06-07 00:16:47 +0100

    API Owners Controller

---
+++ MyLeasing/Controllers/API/OwnerController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyLeasing.Web.Data;
+
+namespace MyLeasing.Web.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OwnersController : Controller
+    {
+        private readonly IOwnerRepository _ownerRepository;
+
+        public OwnersController(IOwnerRepository ownerRepository)
+        {
+            _ownerRepository = ownerRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetOwners()
+        {
+            return Ok(_ownerRepository.GetAll());
+        }
+    }
+}
